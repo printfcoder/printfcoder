4c061403ec4faaea08622b88dd324eb5d80a681d
shu xian <i@shux.me>
2021-11-23 19:22:36 +0800

    two sum sort

--- job/interview/leetcode/array/array_test.go
+++ job/interview/leetcode/array/array_test.go
@@ -6,12 +6,12 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestTwoSum(t *testing.T) {
+func TestTwoSumWithMap(t *testing.T) {
 	Convey("Testing two sum", t, func() {
-		x, y := twoSum(6, 2, 4, 5)
+		x, y := twoSumWithMap(6, 2, 4, 5)
 		So(x, ShouldEqual, 0)
 		So(y, ShouldEqual, 1)
-		x, y = twoSum(9, 2, 4, 5, 9)
+		x, y = twoSumWithSorting(9, 2, 4, 5, 9)
 		So(x, ShouldEqual, 1)
 		So(y, ShouldEqual, 2)
 	})
--- job/interview/leetcode/array/twosum.go
+++ job/interview/leetcode/array/twosum.go
@@ -1,12 +1,17 @@
 package main
 
+import "sort"
+
 // 找出input中两个数相加得target的值，返回坐标
 // 不使用暴力计算，也就是O(N²)的
-// 使用map来搞，O(N)
-func twoSum(target int, input ...int) (x, y int) {
+// 使用map来搞，O(N) Time，O(N) Space
+// 省时，但是费空间
+func twoSumWithMap(target int, input ...int) (x, y int) {
 	// 忽略长度检查
 	// 忽略边界检查
+	// O(N) Space
 	stored := map[int]int{}
+	// O(N) time
 	for i, num := range input {
 		// 记录当前值的匹配值，然后去map里找，如果没有就把自己塞进去，让别人能找到
 		tempNum := target - num
@@ -17,5 +22,30 @@ func twoSum(target int, input ...int) (x, y int) {
 
 		stored[num] = i
 	}
+	return
+}
+
+// 使用排序来搞，O(NlogN) Time，O(1) Space
+// 费时，但是省空间
+func twoSumWithSorting(target int, input ...int) (x, y int) {
+	// 忽略长度检查
+	// 忽略边界检查
+
+	// 排序，然后两端夹逼
+	// 夹逼值大于目标值则end向前进一位
+	// 小于则start后进一位
+	sort.Ints(input)
+	start, end := 0, len(input)-1
+	for start < end {
+		tempSum := input[start] + input[end]
+		if tempSum == target {
+			return start, end
+		} else if tempSum < target {
+			start++
+		} else {
+			end--
+		}
+	}
+
 	return
 }
