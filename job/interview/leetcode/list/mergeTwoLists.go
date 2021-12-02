dada32a0cf7d916b2ef51819d34b6fd1cb78d72e
shu xian <i@shux.me>
2021-12-02 12:56:05 +0800

    mergeTwoLists

---
+++ job/interview/leetcode/list/mergeTwoLists.go
@@ -0,0 +1,50 @@
+package main
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+//
+//
+// 示例 2：
+//
+//
+//输入：l1 = [], l2 = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+// Related Topics 递归 链表
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) (output *ListNode) {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	} else if l1.val < l2.val {
+		l1.next = mergeTwoLists(l1.next, l2)
+		return l1
+	} else {
+		l2.next = mergeTwoLists(l1, l2.next)
+		return l2
+	}
+}
