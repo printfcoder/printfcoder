e8ebf26920d8f742376d53d2761e144d491b339a
shu xian <i@shux.me>
2021-12-13 22:55:19 +0800

    merge

---
+++ job/interview/leetcode/array/merge_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMergeWithTwoPointsFromHead(t *testing.T) {
+	Convey("Testing MergeWithTwoPointsFromHead", t, func() {
+		sorted := mergeWithTwoPointsFromHead([]int{1, 2, 2, 4, 4, 8}, []int{3, 5, 7, 9, 10})
+		So(sorted, ShouldResemble, []int{1, 2, 2, 3, 4, 4, 5, 7, 8, 9, 10})
+	})
+}
+
+func TestMergeWithTowPointsFromTail(t *testing.T) {
+	Convey("Testing MergeWithTowPointsFromTail", t, func() {
+		sorted := mergeWithTowPointsFromTail([]int{1, 2, 2, 4, 4, 8}, []int{3, 5, 7, 9, 10})
+		So(sorted, ShouldResemble, []int{1, 2, 2, 3, 4, 4, 5, 7, 8, 9, 10})
+	})
+}
