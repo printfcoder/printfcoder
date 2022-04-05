edc4c9c402752b69eb362a2ac1169854e5b28026
printfcoder <i@shux.me>
2022-04-05 15:42:50 +0800

    update emc course

--- course/for-emc/src/helloworld/helloworld.go
+++ course/for-emc/src/helloworld/helloworld.go
@@ -2,18 +2,6 @@ package main
 
 import "fmt"
 
-type Abc struct {
-	A int
-	B int
-	C int
-}
-
 func main() {
-	b := make([]int, 0)
-	c := make(map[int]int, 0)
-	var s Abc
-	s.A = 1
-	s.B = 2
-	fmt.Printf("%d %d %d\n", len(b), cap(b), len(c))
 	fmt.Println("Hello World")
 }
